1dbec68e703cf5d22b7a62a09dddfc1b918922e5
Nicole DePergola <281976283+ndepergo@users.noreply.github.com>
2026-06-11 15:58:04 -0400

    working demo v2
    - Created consumer.properties-template for Kafka consumer configuration.
    - Implemented consumer.py to read messages from the DDM_Payload topic, save payloads to files, and calculate latency.
    - Created producer.properties-template for Kafka producer configuration.
    - Implemented producer.py to read DDM payloads from a file and send them to the DDM_Payload topic with metadata.
    - Added requirements.txt to specify confluent-kafka dependency.

---
+++ kafka/python-demo-ddm-payload/consumer.py
@@ -0,0 +1,71 @@
+import hashlib
+import json
+import time
+from confluent_kafka import Consumer
+
+
+topic = "DDM_Payload"   # NOTE The topic which the messages will be received from, rename accordingly to whatever topic you are using
+consumer_group = "gbt-group"  # NOTE rename to whatever consumer group name you want to use
+
+
+def read_config():
+  # reads the client (consumer) configuration from consumer.properties
+  # and returns it as a key-value map
+  config = {}
+  with open("consumer.properties") as fh:
+    for line in fh:
+      line = line.strip()
+      if len(line) != 0 and line[0] != "#":
+        parameter, value = line.strip().split('=', 1)
+        config[parameter] = value.strip()
+  return config
+
+
+def consume(topic, config):
+  # sets the consumer group ID and offset
+  config["group.id"] = consumer_group
+  config["auto.offset.reset"] = "earliest"
+
+  # creates a new consumer instance
+  consumer = Consumer(config)
+
+  # subscribes to the specified topic
+  consumer.subscribe([topic])
+
+  try:
+    while True:
+      # consumer polls the topic and prints any incoming messages
+      msg = consumer.poll(1.0) # polls for messages for 1 second
+      if msg is not None and msg.error() is None:
+        key = msg.key().decode("utf-8")
+        value = msg.value()
+        headers = dict(msg.headers() or []) # 
+        meta = json.loads(headers.get("DDM metadata", b"{}").decode("utf-8")) # loading the DDM metadata from the message headers, which is stored as a JSON string. We decode it from bytes to a string and then load it into a dictionary so we can access the individual fields.
+
+        digest = hashlib.sha256(value).hexdigest()
+        filename = f"received-DDM-{digest:.15}.png" # creating a unique filename for the received DDM payload using a random UUID. You can change this to whatever naming convention you want.
+        
+        with open(filename, 'wb') as file: # writing the received DDM payload to a file in bytes format.
+          file.write(value)
+          
+
+        send_time = float(meta.get('created_timestamp')) # extract timestamp from header
+
+        latency_ms = (time.time() - send_time)*1000 #calculate latency in ms
+
+        print(f"Saved DDM for obs_id '{meta.get('obs_id')}' as {filename} ({len(value)} bytes). Image created at {meta.get('created_timestamp')} by {meta.get('source')}. Latency: {latency_ms:.2f} ms") 
+
+  except KeyboardInterrupt: 
+    pass
+  finally:
+    # closes the consumer connection
+    consumer.close()
+
+
+def main():
+  config = read_config()
+
+  consume(topic, config)
+
+
+main()
---
+++ kafka/python-demo-ddm-payload/producer.py
@@ -0,0 +1,61 @@
+import json
+import time
+from confluent_kafka import Producer
+
+
+topic = "DDM_Payload"  # NOTE The topic to which the messages will be sent, rename accordingly to whatever topic you want to send the DDM payloads to.
+
+
+with open('DDM.png', 'rb') as file: # reading in the DDM payload as bytes from a file. You can replace this with however you are getting the DDM payload, just make sure it's in bytes format.
+  DDM = file.read()
+
+
+def read_config():
+  # reads the client (producer) configuration from producer.properties
+  # and returns it as a key-value map
+  config = {}
+  with open("producer.properties") as fh:
+    for line in fh:
+      line = line.strip()
+      if len(line) != 0 and line[0] != "#":
+        parameter, value = line.strip().split('=', 1)
+        config[parameter] = value.strip()
+  return config
+
+bytes = len(DDM)
+
+metadata = {
+    "obs_id":"ddm-001",
+    "source":"DSOC", 
+    "type/format":"image/png",
+    "created_timestamp": f"{time.time()-5}", # 
+    "message_timestamp": f"{time.time()}",
+    "bytes": f"{bytes}"
+    }
+meta_bytes = json.dumps(metadata).encode("utf-8")
+
+
+key = metadata["obs_id"].encode("utf-8")
+headers = [("DDM metadata", meta_bytes)] 
+value_bytes = DDM # the value of the message is the DDM payload, which is read in as bytes from the file. We don't need to encode it as we did with the JSON string because it's already in bytes format.
+print(f"Size of DDM payload: {len(value_bytes)} bytes.") # check size before sending
+
+def produce(topic, config, key, value, headers):
+  # creates a new producer instance
+  producer = Producer(config)
+
+  # producing a message to the specified topic with the DDM payload as the value. The key is set to None since we are not using it in this example, but you could set it to something if you wanted to. Just make sure to encode it as bytes like we do with the value.
+  producer.produce(topic, key=key, value=value, headers=headers) 
+  print(f"Produced payload to topic {topic}.")
+
+  # send any outstanding or buffered messages to the Kafka broker
+  producer.flush()
+
+
+def main():
+  config = read_config()
+
+  produce(topic, config, key, value_bytes, headers)
+
+
+main()
