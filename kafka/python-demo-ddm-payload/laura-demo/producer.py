4e62327f2c05253639072b9251aae610dbc4a778
lcallahan-nrao <lcallaha@nrao.edu>
2026-06-11 14:49:12 +0000

    Added a timestamp to the message payload
    Added timestamp will be used to calculate latency

--- kafka/python-demo-ddm-payload/laura-demo/producer.py
+++ kafka/python-demo-ddm-payload/laura-demo/producer.py
@@ -1,7 +1,8 @@
 from confluent_kafka import Producer
+import time
+import json
 
 topic = "DDM_png"  # NOTE The topic to which the messages will be sent, rename accordingly
-
 
 def read_config():
   # reads the client (producer) configuration from producer.properties
@@ -16,12 +17,17 @@ def read_config():
   return config
 
 
-def produce(topic, config, key, value):
+def produce(topic, config, key, value, headers):
   # creates a new producer instance
   producer = Producer(config)
-  
+
   # produces a sample message
-  producer.produce(topic, key=key, value=value)
+  producer.produce(
+    topic=topic, 
+    key=key, 
+    value=value,
+    headers=headers)
+  
   print(f"Produced image to topic {topic}")
 
   # send any outstanding or buffered messages to the Kafka broker
@@ -34,7 +40,14 @@ def main():
   with open('DDM1.png', 'rb') as file:
     image_bytes = file.read()
 
-  produce(topic, config, "image", image_bytes)
+  produce(
+    topic, 
+    config, 
+    "image2", 
+    image_bytes,
+    headers={
+      "send_time": str(time.time())# sends a timestamp of the time the message was sent
+    })
 
 
 main()
