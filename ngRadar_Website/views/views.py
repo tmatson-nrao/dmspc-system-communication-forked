49a6fcbd94881513644d5f4fce8228f54901706d
Nicole DePergola <281976283+ndepergo@users.noreply.github.com>
2026-07-09 12:07:05 -0400

    csrf login fix

--- ngRadar_Website/views/views.py
+++ ngRadar_Website/views/views.py
@@ -25,7 +25,6 @@ load_dotenv(override=True)
 #program constants
 DATE_TIME_STRING=19
 
-@cache_control(no_cache=True, must_revalidate=True, no_store=True) #Desmond's Auth token fix - comment if we decide not to use
 def login_view(request):
     if request.method == 'POST':
         username_input = request.POST['username']
