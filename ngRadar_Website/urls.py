fb67d4eeb02524d5e2c3736be6b730920d143238
tnguyen-nrao <tnguyen@nrao.edu>
2026-07-13 09:53:54 -0400

    Add lock_status endpoint for submit lock

--- ngRadar_Website/urls.py
+++ ngRadar_Website/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('home/gbtevent', views.gbt_event_partial, name='gbt_events'),
     path('home/dsocevent', views.dsoc_event_partial, name='dsoc_events'),
     path('submit-waveform/', views.submit_waveform, name='submit_waveform'),
+    path('home/lock-status/',views.lock_status, name ='lock_status'),
+
 
     # Dashboard page URLs
     path('dashboard/', views.dashboard_view, name='dashboard_home'),
